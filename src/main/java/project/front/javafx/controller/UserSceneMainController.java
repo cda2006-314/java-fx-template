cba62a1138c6a63a2f41f9b056fe11f77d54993e
Charlene19 <charlene.romain@yahoo.fr>
2020-11-20 10:36:35 +0100

    UserSceneMainController = list() pour peupler le tableau

--- src/main/java/project/front/javafx/controller/UserSceneMainController.java
+++ src/main/java/project/front/javafx/controller/UserSceneMainController.java
@@ -20,10 +20,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import project.back.back.model.Member;
 import project.back.back.services.EmployeeServices;
 import project.back.back.services.MemberServices;
 import project.front.javafx.FXMLDocumentController;
@@ -37,10 +39,23 @@ import project.front.javafx.Navigation;
 
  @Controller
 public class UserSceneMainController implements Initializable {
-@FXML
-private TableColumn user_AllUsers_TableColumn1;
-@FXML
-private TableColumn user_AllUsers_TableColumn2;
+
+     //Typage de notre tableau et des colonnes
+     @FXML
+     private TableView<Member> user_AllUsers_Table;
+
+     @FXML
+     private TableColumn <Member, Integer> user_AllUsers_TableColumn1;
+     @FXML
+     private TableColumn <Member, String> user_AllUsers_TableColumn2;
+     @FXML
+     private TableColumn <Member, String>  user_AllUsers_TableColumn3;
+     @FXML
+     private TableColumn <Member, Boolean>  user_AllUsers_TableColumn4;
+     @FXML
+     private TableColumn <Member, String>  user_AllUsers_TableColumn5;
+
+
     @FXML
     private Button user_SearchUser_Button;
     @FXML
@@ -51,8 +66,7 @@ private TableColumn user_AllUsers_TableColumn2;
     private ComboBox<?> user_SearchStatus_CB;
     @FXML
     private AnchorPane user_ResultSearch_AnchorPane;
-    @FXML
-    private TableView<?> user_AllUsers_Table;
+
     @FXML
     private Button Back_Button;
      @Autowired
@@ -74,11 +88,23 @@ private TableColumn user_AllUsers_TableColumn2;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
-
-         List listPrep = (List) memberServices.listMemberStatus();
-
+        //Liste tous les status des membres
+        List listPrep = (List) memberServices.listMemberStatus();
         ObservableList list = (ObservableList) FXCollections.observableArrayList(listPrep);
         user_SearchStatus_CB.setItems(list);
+
+        //Peuplement du tableau: tableau éditable permettant la modification
+        user_AllUsers_TableColumn1.setCellValueFactory(new PropertyValueFactory<>("memberId"));
+        user_AllUsers_TableColumn2.setCellValueFactory(new PropertyValueFactory<>("memberEmail"));
+        user_AllUsers_TableColumn3.setCellValueFactory(new PropertyValueFactory<>("memberUsername"));
+        user_AllUsers_TableColumn4.setCellValueFactory(new PropertyValueFactory<>("memberIsverified"));
+        user_AllUsers_TableColumn5.setCellValueFactory(new PropertyValueFactory<>("memberHasMemberstatusesByMemberId"));
+
+        //Construction de notre liste de peuplement
+        ObservableList<Member> listMember = listMember();
+        user_AllUsers_Table.setItems(listMember);
+      //  user_AllUsers_Table.getColumns().addAll(user_AllUsers_TableColumn1, user_AllUsers_TableColumn2, user_AllUsers_TableColumn3, user_AllUsers_TableColumn4, user_AllUsers_TableColumn5);
+
 
 
         AquaFx.createButtonStyler().setType(ButtonType.ROUND_RECT).style(user_SearchUser_Button);
@@ -97,6 +123,14 @@ private TableColumn user_AllUsers_TableColumn2;
                 Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
-    }    
-    
-}
+    }
+
+     private ObservableList<Member> listMember() {
+
+
+
+        ObservableList<Member> listTable = FXCollections.observableArrayList(memberServices.listMember());
+
+    return listTable;}
+
+ }
