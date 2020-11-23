316618eb5f04f9704b45772b78b1393f8f4b7c69
Charlene19 <charlene.romain@yahoo.fr>
2020-11-23 12:21:32 +0100

    unavailable tests

--- src/test/java/project/back/SpringJavaFXApplicationTests.java
+++ src/test/java/project/back/SpringJavaFXApplicationTests.java
@@ -1,6 +1,6 @@
 package project.back;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
