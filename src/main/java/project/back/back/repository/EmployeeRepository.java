8abb88364e335dedcd16194f39b4ff754d3447db
Charlene19 <charlene.romain@yahoo.fr>
2020-11-24 17:25:58 +0100

    pb avec la vue ConfigAdmin.fxml

--- src/main/java/project/back/back/repository/EmployeeRepository.java
+++ src/main/java/project/back/back/repository/EmployeeRepository.java
@@ -3,5 +3,9 @@ package project.back.back.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import project.back.back.model.Employee;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    Optional<Employee> findEmployeeByEmployeeLogin(String login);
 }
