0dc023f1b5930ada92b66ec596c4a4fdc5d8eb74
zvrsd <65198569+zvrsd@users.noreply.github.com>
2020-11-27 11:23:18 +0100

    HTTP upload test method

--- src/main/java/project/back/back/util/ApiAdaptaterClient.java
+++ src/main/java/project/back/back/util/ApiAdaptaterClient.java
@@ -1,8 +1,18 @@
 package project.back.back.util;
 
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -19,6 +29,9 @@ public class ApiAdaptaterClient {
     private static String filename;
 
     public static void main(String[] args) {
+        sendFile("src/main/resources/application.properties");
+
+        /*
         try {
 
             URL url = new URL(JSON_URL);//your url i.e fetch data from .
@@ -46,5 +59,32 @@ public class ApiAdaptaterClient {
         } catch (Exception e) {
             System.out.println("Exception in NetClientGet:- " + e);
         }
+        */
+    }
+
+    private static void sendFile(String filepath){
+
+        // Open a file that is going to be sent
+        File file = new File(filepath);
+        FileSystemResource fileSystemResource = new FileSystemResource(file);
+
+        // Setup headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+        headers.add("api-key", "49a8z4e98a4ze");
+
+        // Body building - including the file above
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        body.add("file", fileSystemResource);
+
+        // Request processing
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        String serverUrl = "http://localhost:8090/api/v1/media/upload";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(serverUrl, requestEntity, String.class);
+
+        // Displays the response
+        System.out.println("Response code: " + response.getStatusCode());
+        System.out.println("Response :\n"+response.getBody());
     }
 }
